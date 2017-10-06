936f12d21a8b6226d42a0862dc23eaf114ba0fde
GiovannaMelchiorre <giovanna.melchiorre.92@gmail.com>
2017-10-06 12:44:53 +0200

    mario rossi2

--- drones/database/seeds/MaitreTransportSeeder.php
+++ drones/database/seeds/MaitreTransportSeeder.php
@@ -12,7 +12,7 @@ class MaitreTransportSeeder extends Seeder
     public function run()
     {
         DB::table('maitre_transports')->insert([
-            'name' => 'MarioRr',
+            'name' => 'MarioRdfghr',
             'surname' => 'Rossi',
             'created_at' => Carbon\Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon\Carbon::now()->format('Y-m-d H:i:s'),
