6be21fd6937cc079de2c22da482b3a28f845277f
Matteo Polsinelli <netrider91@gmail.com>
2017-10-19 19:08:57 +0200

    fix migrations path

---
+++ drones/database/migrations/2017_10_19_165708_add_columns_to_paths_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToPathsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('paths', function (Blueprint $table) {
+            $table->geometry('path_geometry');
+			$table->float('path_lenght', 3, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('paths', function (Blueprint $table) {
+            //
+        });
+    }
+}
