3651d3255743609f686ab46636810328ab13d70f
Davide Giancola <gia.davide@gmail.com>
2017-10-19 17:26:45 +0200

    Carrier completo

--- drones/app/Services/OrderService.php
+++ drones/app/Services/OrderService.php
@@ -8,15 +8,15 @@ use App\Models\TransportMaitre;
 class OrderService
 {
     public function newOrderTransport() {
-		//Qui andrà la sessione. Ciò prevede che l'utente si sia registrato,
-		//loggato. Dato che siamo nella prima iterazione queste funzionalità
-		//non sono ancora state implementate. Per questo motivo scrivo un id, che
-		//sicuramente esiste perché sono stati creati i seeds, direttametne a mano.
-		$transportMaitre = \App\Models\TransportMaitre::find(1);
-		$transportOrder = new TransportOrder();
-		$transportOrder->setMaitre($transportMaitre);
-		$transportOrder->save();
-	}
+        //Qui andrà la sessione. Ciò prevede che l'utente si sia registrato,
+        //loggato. Dato che siamo nella prima iterazione queste funzionalità
+        //non sono ancora state implementate. Per questo motivo scrivo un id, che
+        //sicuramente esiste perché sono stati creati i seeds, direttametne a mano.
+        $transportMaitre = \App\Models\TransportMaitre::find(1);
+        $transportOrder = new TransportOrder();
+        $transportOrder->setMaitre($transportMaitre);
+        $transportOrder->save();
+    }
 
     public function consignCarriers($carriersList){
 
