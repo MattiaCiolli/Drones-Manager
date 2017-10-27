bd3774c489dea4b216336936d58e4b105f1b10d6
MattiaCiolli <mattia93c@gmail.com>
2017-10-27 12:09:52 +0200

    show discount

--- drones/app/Http/ViewComposers/SummaryComposer.php
+++ drones/app/Http/ViewComposers/SummaryComposer.php
@@ -8,6 +8,7 @@ use App\Services\ProductService;
 class SummaryComposer
 {
     private $order;
+
     /**
      * Create a movie composer.
      *
@@ -15,17 +16,33 @@ class SummaryComposer
      */
     public function __construct()
     {
-        $transportOrder= \App\Models\TransportOrder::find(1);
+        $transportOrder = \App\Models\TransportOrder::find(1);
         //accedere a config
-        $this->order[0]= $transportOrder;
-        //fare parse per controllare valore dello sconto
-        $this->order[1]= config('price.discountStrategies.quantity');
+        $this->order[0] = $transportOrder;
+        $d = array();
+        for ($i = 0; $i < strlen($transportOrder->price->discount); $i++) {
+            $char = substr($transportOrder->price->discount, $i, 1);
+            if (strpos($char, 'P') !== false) {
+                $d[$i][0]= "Sconto lunghezza percorso:";
+                $d[$i][1]= config('price.discountStrategies.pathLength')."€";
+            } elseif (strpos($char, 'Q') !== false) {
+                $d[$i][0]= "Sconto quantità:";
+                $d[$i][1]= $this->percentage(config('price.discountStrategies.quantity'));
+            }
+        }
+        $this->order[1] = $d;
+    }
+
+    public function percentage($in)
+    {
+        $in=100 - ($in*100);
+        return $in."%";
     }
 
     /**
      * Bind data to the view.
      *
-     * @param  View  $view
+     * @param  View $view
      * @return void
      */
     public function compose(View $view)
