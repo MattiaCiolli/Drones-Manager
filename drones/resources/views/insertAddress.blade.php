3b18ef33396889b29746af43c37b8109de4f03af
GiovannaMelchiorre <giovanna.melchiorre.92@gmail.com>
2017-10-31 17:17:02 +0100

    scritte agli indirizzi

--- drones/resources/views/insertAddress.blade.php
+++ drones/resources/views/insertAddress.blade.php
@@ -59,8 +59,8 @@
                             <i class="fa fa-home"></i>
                         </li>
                         <li>
-                            <span id="spanCheckOk" class=" glyphicon glyphicon-ok text-success"></span>
-                            <span id="spanCheckNo" class="glyphicon glyphicon-remove text-danger"></span>
+                            <span id="spanCheckOk" class=" glyphicon glyphicon-ok text-success"> Indirizzo corretto!</span>
+                            <span id="spanCheckNo" class="glyphicon glyphicon-remove text-danger"> Indirizzo errato! Riprovare! </span>
                         </li>
                     </ol>
                 </div>
